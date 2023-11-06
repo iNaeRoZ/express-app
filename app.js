bae3a601313455be273343227995b129ed620010
iNaeRoZ <naeroz@outlook.fr>
2023-11-06 10:12:29 +0100

    bug corriger

--- app.js
+++ app.js
@@ -55,3 +55,15 @@ const movies = [
     duration: 180,
   },
 ];
+
+
+
+const port = 5000;
+
+app
+  .listen(port, () => {
+    console.info(`Server is listening on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Error:", err.message);
+  });
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const app = require("./app");
-
 const port = 5000;
 
 app
