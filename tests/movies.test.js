2f274def6aded1576ab3a96c387fe09377f780e8
iNaeRoZ <naeroz@outlook.fr>
2023-11-06 10:23:34 +0100

    test accompli

---
+++ tests/movies.test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const app = require("../app");
+const { describe } = require("node:test");
+
+describe("GET /api/movies", () => {
+  it("should return all movies", async () => {
+    const response = await request(app).get("/api/movies");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("GET /api/movies/1", () => {
+  it("should return one movie", async () => {
+    const response = await request(app).get("/api/movies/1");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
